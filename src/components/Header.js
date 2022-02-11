b07f507636a2369add4f736e646d80e2d2113ce6
CAR-NADO <mdzeeshan787898@gmail.com>
2022-02-11 11:51:06 +0530

    pharma

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="left-header">
+                <p>Express US to US 3 Days Delivery</p>
+            </div>
+            
+            <div className="right-header">
+                <p>Login / Sign up</p>
+            </div>
+        </header>
+    )
+};
+
+export default Header;
